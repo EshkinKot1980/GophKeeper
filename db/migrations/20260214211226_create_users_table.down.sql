825d8a549fff8b1104682a6651bba12a9de4686a
Aleksey Kamyrin <akamyrin@yandex.ru>
2026-02-18 20:29:13 +0000

    data добавил мирации для таблицы секретов

--- db/migrations/20260214211226_create_users_table.down.sql
+++ db/migrations/20260214211226_create_users_table.down.sql
@@ -1,1 +1,1 @@
-DROP TABLE users;
+DROP TABLE users IF EXISTS;
---
+++ db/migrations/20260218144405_create_secrets_table.up.sql
@@ -0,0 +1,19 @@
+BEGIN TRANSACTION;
+
+CREATE TYPE secret_data_type AS ENUM ('credentials', 'card', 'file', 'text');
+
+CREATE TABLE IF NOT EXISTS secrets (
+    -- Использются чиловое значение, потому что пользователю удобнее вводить чем UUID
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    data_type secret_data_type NOT NULL,
+    title VARCHAR(32) NOT NULL,
+    meta_data JSONB NOT NULL DEFAULT '{}'::jsonb,
+    encripted_data BYTEA,
+    ecrypted_key VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS idx_secrets_user_id_data_type ON secrets(user_id, data_type);
+COMMIT;
